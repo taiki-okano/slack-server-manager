770056e2c486fa77ecefa5715aa1b6a9d2d75329
Taiki Okano <taiki@okano.online>
2018-12-31 01:24:39 +0900

    Update .gitignore
    Add slackbot_settings.py to .gitignore because of an API token issue.

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 # Bot token
-API_TOKEN = 'xoxb-3773716512-513698063861-VAVlPToinFpwBWmFYX5lD99I'
+API_TOKEN = 'xoxb-3773716512-513698063861-nlShEiEnzFFwcaVGREQqx07A'
 
 # Default reply
 DEFAULT_REPLY = 'Error: No such command'
